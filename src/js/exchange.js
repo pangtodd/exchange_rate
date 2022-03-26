2ebf18a2da68581ad1bcb370c26546f3e1f76002
Todd Pangilinan <pang.todd@gmail.com>
2022-03-26 08:36:47 -0700

    add new static asnych function getFull to exhangeService, which pulls the full curency information instead of just data for a pair of currencies.

--- src/js/exchange.js
+++ src/js/exchange.js
@@ -2,7 +2,6 @@ export default class ExchangeService {
   static async getExchange(currencyType) {
     try {
       let url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currencyType}`;
-      console.log(url);
       const response = await fetch (url);
       console.log(response);
       if (!response.ok){
@@ -13,4 +12,17 @@ export default class ExchangeService {
       return error.message;
     }
   }
+  static async getFull() {
+    try {
+      let url2 = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+      const response2 = await fetch (url2);
+      console.log("response 2 : "+response2);
+      if (!response2.ok){
+        throw Error(response2.statusText);
+      }
+      return response2.json();
+    } catch(error) {
+      return error.message;
+    }
+  } 
 }
